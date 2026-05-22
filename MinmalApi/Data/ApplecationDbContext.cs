a6b9f6322dc684f48881f43293c138bfa2adb76a
Amr <amrhoji77@gmail.com>
2026-05-22 14:47:37 +0300

    first deploy

---
+++ MinmalApi/Data/ApplecationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinmalApi.Model;
+
+namespace MinmalApi.Data
+{
+    public class ApplecationDbContext : DbContext
+    {
+        public ApplecationDbContext(DbContextOptions<ApplecationDbContext> options) : base(options) { }
+
+
+        public DbSet<Employee> Employees { get; set; }
+
+
+
+
+    }
+}
---
+++ MinmalApi/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MinmalApi.Data;
+using MinmalApi.Model;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MinmalApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+          /*  builder.Services.AddAuthorization();
+            builder.Services.AddDbContext<ApplecationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+*/
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+
+            app.MapGet("/", () =>
+            {
+                return "Hello Azure";
+            });
+
+
+            app.Run();
+        }
+    }
+}
